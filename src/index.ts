c94d74e7f6a1a019526ee6bc76a6c92d6e3f0c4c
tru69er <chidori9912@gmail.com>
2023-09-04 01:58:20 +0530

    Moved handler function to route file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from "express"
-
 import bodyParser from "body-parser"
 import { configDotenv } from "dotenv"
 import cors from "cors"
-import { Stripe } from "stripe"
-
-import users from "./models/users"
 import mongoose from "mongoose"
+
+import register from "./routes/register"
 
 configDotenv()
 
@@ -17,55 +15,12 @@ async function main() {
         console.log("Connected to DB")
     }
 
-    let stripe: Stripe;
-
-    if (process.env.STRIPE_PK) {
-        stripe = new Stripe(process.env.STRIPE_PK, {
-            apiVersion: "2023-08-16",
-        })
-        if (!stripe) {
-            console.log("Stripe connection failed")
-            process.exit(1)
-        }
-        console.log("Connected to Stripe")
-    }
-
     const app = express()
     app.use(bodyParser.json())
     app.use(cors())
 
-    app.post("/register", async (req, res) => {
-        const { name, email, password } = req.body
-
-        if (await users.findOne({ email })) {
-            res.writeHead(400)
-        } else {
-            const customer = await stripe.customers.create({
-                description: "",
-                name,
-                email,
-            })
-            if (!customer) {
-                res.writeHead(500)
-            } else {
-                if (!await users.create({
-                    name,
-                    email,
-                    password,
-                    plan: 0,
-                    stripeID: customer.id,
-                    substate: "None",
-                    billing: 0,
-                    startDate: Date.now(),
-                })) {
-                    res.writeHead(500)
-                } else {
-                    res.writeHead(200)
-                }
-            }
-        }
-        res.end()
-    })
+    //assign register route to handler
+    app.use("/register", register)
 
     app.listen(6969, () => { console.log("App started in http://localhost:6969") })
 }
---
+++ src/routes/register.ts
@@ -0,0 +1,65 @@
+//New User Registration route
+
+import { Router } from "express";
+import { Stripe } from "stripe"
+import { configDotenv } from "dotenv"
+
+import users from "../models/users"
+
+//Intialize config to access variables from .env file
+configDotenv()
+
+const router = Router()
+let stripe: Stripe
+//Initialize stripe object
+if (process.env.STRIPE_PK) {
+    stripe = new Stripe(process.env.STRIPE_PK, {
+        apiVersion: "2023-08-16",
+    })
+    if (!stripe) {
+        console.log("Stripe connection failed")
+        process.exit(1)
+    }
+}
+
+router.post("/", async (req, res) => {
+    //get creds from request body
+    const { name, email, password } = req.body
+
+    if (await users.findOne({ email })) { //check if email already exists
+        res.writeHead(400)
+    } else {
+
+        //create stripe customer
+        const customer = await stripe.customers.create({
+            description: "",
+            name,
+            email,
+        })
+
+        //error from stripe in creating customer
+        if (!customer) {
+            res.writeHead(500)
+        } else {
+            //add to db and handle error
+            if (!await users.create({
+                name,
+                email,
+                password,
+                plan: 0,
+                stripeID: customer.id,
+                substate: "None",
+                billing: 0,
+                startDate: Date.now(),
+            })) {
+                res.writeHead(500)
+            } else {
+                res.writeHead(200)
+            }
+        }
+    }
+    res.end()
+
+})
+
+export default router
