f8ee7b957d94d5ffae61c6f69fe57e8572345861
tru69er <chidori9912@gmail.com>
2023-09-04 11:04:29 +0530

    Added store route documentation

--- src/routes/store.ts
+++ src/routes/store.ts
@@ -7,16 +7,22 @@ import users from "../models/users"
 let router = Router()
 
 router.post("/", async (req, res) => {
+    //Read email to find user in DB, plan id, and billing id to store in DB
     const { email, plan, billing } = req.body
 
+    //Search for user in DB
     const user = await users.findOne({ email })
 
     if (!users) {
+        //Error should never occur, recheck frontend if it does
         res.writeHead(404)
     } else {
-        const updated = await users.updateOne(
+
+        //update user's plan details
+        const update = await users.updateOne(
             { email },
             {
+                //properties of user document to be updated, other properties will remain unchanged
                 $set: {
                     plan,
                     substate: "Active",
@@ -25,7 +31,8 @@ router.post("/", async (req, res) => {
                 },
             })
 
-        if (!updated.acknowledged) {
+        //If update was unsuccessful
+        if (!update.acknowledged) {
             res.writeHead(500)
         } else {
             res.writeHead(200)
