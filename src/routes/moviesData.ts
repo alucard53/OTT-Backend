4774d5639f73a5e7595d07c3feda5410c684c0aa
shinchan <paryushidev0909@gmail.com>
2023-09-10 22:08:46 +0530

    search

--- src/routes/moviesData.ts
+++ src/routes/moviesData.ts
@@ -3,27 +3,17 @@ import movies from "../models/movies";
 
 const router = Router();
 
-router.post("/", async (req, res) => {
-  const title = req.body;
+router.get("/", async (req, res) => {
+  //   const title = req.body;
 
-  let movie = await movies.findOne({ title });
+  let movie = await movies.find();
   console.log(movie);
 
   if (!movie) {
     res.write(404);
     console.log("Movie not found!");
   } else {
-    res.write(200);
-    res.write(
-      JSON.stringify({
-        id: movie.id,
-        title: movie.title,
-        year: movie.year,
-        genre: movie.genre,
-        director: movie.director,
-        desc: movie.desc,
-      })
-    );
+    res.write(JSON.stringify(movie));
   }
   res.end();
 });
