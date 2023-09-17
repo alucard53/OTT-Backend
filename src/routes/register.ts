c9b8a8c1715c960026c0c234c7089ba3a61d969c
tru69er <chidori9912@gmail.com>
2023-09-17 21:52:44 +0530

    .env fix

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -2,63 +2,68 @@
 
 import { Router } from "express"
 import { Stripe } from "stripe"
+import { configDotenv } from "dotenv"
 
 import users from "../models/users"
 
 //Intialize config to access variables from .env file
 
 const router = Router()
+configDotenv()
+
 let stripe: Stripe
-//Initialize stripe object
 if (process.env.STRIPE_PK) {
-  stripe = new Stripe(process.env.STRIPE_PK, {
-    apiVersion: "2023-08-16",
-  })
-  if (!stripe) {
-    console.log("Stripe connection failed")
-    process.exit(1)
-  }
+    stripe = new Stripe(process.env.STRIPE_PK, {
+        apiVersion: "2023-08-16",
+    })
+    console.log(stripe)
+    if (!stripe) {
+        console.log("Stripe connection failed")
+        process.exit(1)
+    }
+} else {
+    console.log("Stripe PK not found")
 }
 
 router.post("/", async (req, res) => {
-  //get creds from request body
-  const { name, email, password } = req.body
+    //get creds from request body
+    const { name, email, password } = req.body
 
-  console.log(name)
+    console.log(name)
 
-  if (await users.findOne({ email })) { //check if email already exists
-    res.status(400).end()
-    return
-  }
+    if (await users.findOne({ email })) { //check if email already exists
+        res.status(400).end()
+        return
+    }
 
-  //create stripe customer
-  const customer = await stripe.customers.create({
-    description: "",
-    name,
-    email,
-  })
+    //create stripe customer
+    const customer = await stripe.customers.create({
+        description: "",
+        name,
+        email,
+    })
 
-  //error from stripe in creating customer
-  if (!customer) {
-    res.status(500).end()
-    return
-  }
+    //error from stripe in creating customer
+    if (!customer) {
+        res.status(500).end()
+        return
+    }
 
-  //add to db and handle error
-  if (!await users.create({
-    name,
-    email,
-    password,
-    plan: 0,
-    stripeID: customer.id,
-    substate: "None",
-    billing: 0,
-    startDate: Date.now(),
-  })) {
-    res.status(500).end()
-  } else {
-    res.status(200).end()
-  }
+    //add to db and handle error
+    if (!await users.create({
+        name,
+        email,
+        password,
+        plan: 0,
+        stripeID: customer.id,
+        substate: "None",
+        billing: 0,
+        startDate: Date.now(),
+    })) {
+        res.status(500).end()
+    } else {
+        res.status(200).end()
+    }
 
 })
 
